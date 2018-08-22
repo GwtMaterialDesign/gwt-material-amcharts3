7b803934c04ad220ba95f8ec4c2d2e7309130297
mark.baldemor <kevzlou7979@gmail.com>
2018-08-22 19:58:51 +0800

    POC for Date Axis zooming

--- src/main/java/gwt/material/design/amcharts/client/ui/chart/axis/DateAxis.java
+++ src/main/java/gwt/material/design/amcharts/client/ui/chart/axis/DateAxis.java
@@ -1,12 +1,21 @@
 package gwt.material.design.amcharts.client.ui.chart.axis;
 
 import gwt.material.design.amcharts.client.ui.chart.datafields.IDateAxisDataFields;
+import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
+
+import java.util.Date;
 
 @JsType(isNative = true, namespace = "am4charts")
 public class DateAxis extends ValueAxis {
 
     @JsProperty
     public IDateAxisDataFields dataFields;
+
+    @JsMethod
+    public native void zoomToDates(Date startDate, Date endDate);
+
+    @JsMethod
+    public native void zoomToDates(Date startDate, Date endDate, boolean skipRangeEvent, boolean instantly);
 }
--- src/main/java/gwt/material/design/amcharts/client/ui/chart/base/Component.java
+++ src/main/java/gwt/material/design/amcharts/client/ui/chart/base/Component.java
@@ -1,6 +1,7 @@
 package gwt.material.design.amcharts.client.ui.chart.base;
 
 import gwt.material.design.amcharts.client.ui.chart.data.DataSource;
+import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
@@ -9,4 +10,10 @@ public class Component extends Container {
 
     @JsProperty
     public DataSource dataSource;
+
+    @JsMethod
+    public native void zoomToIndexes(int startIndex, int endIndex, boolean skipRangeEvent, boolean instantly);
+
+    @JsMethod
+    public native void zoomToIndexes(int startIndex, int endIndex);
 }
